c78fa6b66a3fe82cb3f8177c37db274e7b2ec491
balajipedapudi <balajipedapudi@gmail.com>
2024-05-27 11:53:58 +0530

    added content to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  ngOninit(){
+    
+  }
+
+  
+  constructor(){
+    var options = {
+      series: [{
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+      chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Product Trends by Month',
+      align: 'left'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+  }
 }
